0a9612812426dd11fa99f2ff663765278550ef64
Carina Milano <carinamilano@gmail.com>
2025-10-25 16:26:26 -0300

    actualice listado de funciones a importar

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 # PROGRAMA PRINCIPAL - ENTRE SABORES
 
-from funciones import generar_archivo_usuarios, log_in, menu_principal, cargar_carta,guardar_stock,cargar_stock,mostrar_stock
+from funciones import (
+    generar_archivo_usuarios,
+    log_in,
+    menu_principal,
+    cargar_carta,
+    guardar_carta,
+    cargar_stock,
+    guardar_stock,
+    registrar_evento,
+    mostrar_carta,
+    submenu_modificar_carta,
+    agregar_plato,
+    guardar_agregar_plato,
+    eliminar_plato,
+    modificar_plato,
+    mostrar_stock,
+    numeroEntreRango,
+    ingresar_num_entero,
+    ingresar_num_mayor_a,
+    ingresar_valor_float_positivo,
+    str_minimo_n_caracteres,
+    tomar_pedido
+)
 
 def main():
     generar_archivo_usuarios()
